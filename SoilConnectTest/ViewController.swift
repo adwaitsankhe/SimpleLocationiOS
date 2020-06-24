7e17bdade17a6ea6060bb2e15ab2b125113e85b4
Adwait Y Sankhe <adwaitsankhe@gmail.com>
2020-06-24 18:54:37 -0400

    Initial Commit

---
+++ SoilConnectTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoilConnectTest
+//
+//  Created by Adwait Y Sankhé on 6/24/20.
+//  Copyright © 2020 Adwait Y Sankhé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
